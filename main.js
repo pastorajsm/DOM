da7925a49cae7626c476b9f50b8b327265657405
Alan Jones <pastorajsm@gmail.com>
2020-06-19 23:26:30 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// const webHeading = document.getElementByClassName('h1')
+// webHeading.append('webPage')
+// parentElement.append(webHeading)
+
+// for (let index = 0; index < image.length; index += 1) {
+//     webHeading[index].addEventListener("click", function () {
+//         this.parentElement.querySelector(".nested").classList.webHeading("active");
+//         this.classList.webHeading("");
+//     });
+// }
+
+const bodyElement = document.body
+const button = document.createElement('button')
+const main = document.createElement('main')
+const img = document.createElement('img')
+const a = document.createElement('a')
+
+bodyElement.append(button)
+bodyElement.append(main)
+main.append(img)
+main.append(a)
+
+img.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSFHB3cZv2Hbc-QA4hxQKek116jWF0hpvrxLwmymfW5cG68S17C&usqp=CAU'
+
+className += "img.add()"
+function myFunction() {
+    const main = document.createElement('anchor');
+    main.className.add('a')
+    document.bodyElement('anchor').href = 'a';
+
+
+}
+
+
+
+
+
+// image needs a class added
+// look up className or classList or setAttribute
+// anchor needs a class added
+// anchor needs a href added
+// append text to button
+// append some text to the anchor
+//button will need addEventListener
+// eventListener function need to remove main
